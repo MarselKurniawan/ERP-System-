33d80572e31ba47c194426ddd16d3a95947d4dae
Leap <git@leap.new>
2025-08-08 06:33:07 +0000

    Manual code change

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -171,7 +171,7 @@ export default function Layout({ children }: LayoutProps) {
         <main className="flex-1 overflow-auto p-6 lg:p-8">
           {children}
         </main>
-				<footer>
+				<footer className="flex items-center ">
 				Copyright @2023 - Marselisius 
 				</footer>
       </div>
